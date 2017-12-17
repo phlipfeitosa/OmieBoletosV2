05f13e8fc4bf36a9d55ac9210da0877652696ec6
philipfeitosa <philip.feitosa@hotmail.com>
2017-12-17 16:35:53 -0200

    Boletos

--- src/br/com/terranova/servlet/ws/BoletoServlet.java
+++ src/br/com/terranova/servlet/ws/BoletoServlet.java
@@ -1,2 +1,23 @@
-public class BoletoServlet extends HttpServlet {  
+package br.com.terranova.servlet.ws;
+
+public class BoletoServlet extends HttpServlet {
+  public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    this.execute(request, response);
+
+  }
+
+  public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    this.execute(request, response);
+
+  }
+
+  public void execute(HttpServletRequest request, HttpServletResponse response) {
+    response.setContentType("application/json");
+    String boletoListarRequest = request.getParameter("BoletoListarRequest");
+
+    BoletoClient boletoClient = new BoletoClient();
+    BoletoListarResponse boletoListarResponse = boletoClient.execute(boletoListarRequest);
+
+  }
+
 }
